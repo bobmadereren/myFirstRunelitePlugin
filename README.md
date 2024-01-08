56e9e63e61256b1d5cc8cc64bab5355f8018b5b9
bobmadereren <williamspurkeland@hotmail.com>
2024-01-08 02:06:12 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Example
-An example greeter plugin
+# My first RuneLite plugin
+I want to make a plugin that streams my grand exchange transactions to a csv or JSON file or possibly directly to
+google sheets if possible.
--- src/test/java/com/myfirstplugin/MyFirstPluginTest.java
+++ src/test/java/com/myfirstplugin/MyFirstPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.myfirstplugin;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class MyFirstPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(MyFirstPlugin.class);
 		RuneLite.main(args);
 	}
 }
