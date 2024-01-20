17af6273c73014d1eb5c4d1b4cb8d10be5050cd4
bobmadereren <williamspurkeland@hotmail.com>
2024-01-20 10:08:54 +0100

    Just some testing stuff

--- src/main/java/com/bobmadereren/myfirstruneliteplugin/MasterPanel.java
+++ src/main/java/com/bobmadereren/myfirstruneliteplugin/MasterPanel.java
@@ -1,29 +1,88 @@
 package com.bobmadereren.myfirstruneliteplugin;
 
+import net.runelite.api.Client;
+import net.runelite.api.widgets.Widget;
 import net.runelite.client.ui.PluginPanel;
 
 import javax.inject.Inject;
 import javax.swing.*;
 import java.awt.*;
+import java.util.Arrays;
 
 public class MasterPanel extends PluginPanel {
 
-    @Inject
-    private MyPlugin plugin;
+    private final Client client;
+    private final MyPlugin plugin;
 
-    MasterPanel(){
+    @Inject
+    MasterPanel(Client client, MyPlugin plugin){
         super();
+        this.client = client;
+        this.plugin = plugin;
         createPanel();
     }
 
     private void createPanel(){
         setBackground(Color.orange);
+
         JButton button = new JButton("I am the button");
-        button.addActionListener(e -> {
-            System.out.println("Clicking button!");
-            plugin.clickButton();
+        button.addActionListener(plugin.setButtonClicked);
+
+        JButton widgetButton = new JButton("Widget");
+        widgetButton.addActionListener(e -> {
+            Widget historyWidget = client.getWidget(383, 3);
+            if(historyWidget != null) {
+                Widget[] widgets = historyWidget.getDynamicChildren();
+                System.out.println("Number of widgets: " + widgets.length);
+                if (widgets.length >= 6) {
+                    Widget priceWidget = widgets[5];
+                    String text = priceWidget.getText();
+                    System.out.println(text);
+                }
+            } else {
+                System.out.println("History Widget is null");
+            }
         });
+
+        JButton historyButton = new JButton("History button");
+        historyButton.addActionListener(e -> {
+            Widget widget = client.getWidget(465, 3);
+            if(widget != null){
+                Object[] objects = widget.getOnOpListener();
+                if(objects != null) {
+                    for (Object object : objects) {
+                        if (object != null) {
+                            System.out.println("Type: " + object.getClass() + ", To string: " + object.toString());
+                        } else {
+                            System.out.println("Object (script / argument) is null");
+                        }
+
+                    }
+                } else {
+                    System.out.println("Object[] is null (has no script)");
+                }
+            } else {
+                System.out.println("History button is null");
+            }
+        });
+
+        JButton changeCollectScript = new JButton("Change collect script");
+        changeCollectScript.addActionListener(e -> {
+            Widget history = client.getWidget(465, 3);
+            Widget collectHead = client.getWidget(465, 6);
+            if(history == null || collectHead == null){
+                System.out.println("History or Collect-Head is null");
+            } else {
+                Object[] historyScript = history.getOnOpListener();
+                collectHead.setOnOpListener(historyScript);
+                System.out.println("New script added to Collect Head: " + Arrays.deepToString(historyScript));
+            }
+        });
+
         add(button);
+        add(widgetButton);
+        add(historyButton);
+        add(changeCollectScript);
     }
 
 }
--- src/main/java/com/bobmadereren/myfirstruneliteplugin/MyPlugin.java
+++ src/main/java/com/bobmadereren/myfirstruneliteplugin/MyPlugin.java
@@ -2,14 +2,11 @@ package com.bobmadereren.myfirstruneliteplugin;
 
 import com.google.inject.Provides;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
-import net.runelite.api.Client;
-import net.runelite.api.GameState;
-import net.runelite.api.events.GameStateChanged;
-import net.runelite.api.events.GameTick;
-import net.runelite.api.events.GrandExchangeOfferChanged;
+import net.runelite.api.*;
+import net.runelite.api.events.*;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.game.ItemManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.ClientToolbar;
@@ -17,6 +14,7 @@ import net.runelite.client.ui.NavigationButton;
 import net.runelite.client.util.ImageUtil;
 
 import javax.inject.Inject;
+import java.awt.event.ActionListener;
 
 @Slf4j
 @PluginDescriptor(
@@ -34,9 +32,12 @@ public class MyPlugin extends Plugin
 	@Inject
 	private PluginConfig config;
 
+	@Inject
+	private ItemManager itemManager;
+
 	private boolean buttonClicked = false;
 
-	protected void clickButton() { buttonClicked = true; }
+	protected ActionListener setButtonClicked = e -> buttonClicked = true;
 
 	@Override
 	protected void startUp() throws Exception
@@ -44,7 +45,6 @@ public class MyPlugin extends Plugin
 		log.info("My first plugin started!");
 
 		MasterPanel masterPanel = injector.getInstance(MasterPanel.class);
-		//MasterPanel masterPanel = new MasterPanel(client, config);
 
 		NavigationButton navButton = NavigationButton.builder()
 				.tooltip("My first plugin!")
@@ -63,34 +63,47 @@ public class MyPlugin extends Plugin
 	}
 
 	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
-	{
+	public void onGameStateChanged(GameStateChanged gameStateChanged) {
 		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
-		{
 			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "My first plugin says " + config.greeting(), null);
-		}
 	}
 
 	@Subscribe
 	public void onGameTick(GameTick gameTick){
 		if(buttonClicked){
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "My first plugin says " + config.greeting(), null);
+			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "",  "Button was clicked", null);
 			buttonClicked = false;
 		}
 	}
 
 	@Subscribe
-	public void onGrandExchangeOfferChanged(GrandExchangeOfferChanged offerChangedEvent) {
-		//printGrandExchangeOffer(offerChangedEvent);
+	public void onItemContainerChanged(ItemContainerChanged itemContainerChanged){
+		ItemContainer i = itemContainerChanged.getItemContainer();
+		System.out.printf("%nContainer modified - Container ID: %d, Space: %,d / %,d%n", i.getId(), i.count(), i.size());
+		System.out.printf("    %-20s%-20s%n    %-20s%-20s%n", "Name", "Quantity", "----", "--------");
+		for(Item item : i.getItems())
+			System.out.printf("    %-20s%,-20d%n", itemManager.getItemComposition(item.getId()).getName(), item.getQuantity());
 	}
 
-	private void printGrandExchangeOffer(GrandExchangeOfferChanged offerChangedEvent){
-		System.out.println("Slot: " + offerChangedEvent.getSlot());
-		System.out.println("State: " + offerChangedEvent.getOffer().getState());
-		System.out.println("Item ID: " + offerChangedEvent.getOffer().getItemId());
-		System.out.println("Price: " + offerChangedEvent.getOffer().getPrice() + " each, " + offerChangedEvent.getOffer().getSpent() + " total");
-		System.out.println("Quantity: " + offerChangedEvent.getOffer().getQuantitySold() + " / " + offerChangedEvent.getOffer().getTotalQuantity());
-		System.out.println("--------------------");
+	@Subscribe
+	public void onMenuOptionClicked(MenuOptionClicked menuOptionClicked){
+		//System.out.printf("%nAction%n    %s%n", menuOptionClicked.getMenuAction());
+	}
+
+	@Subscribe
+	public void onGrandExchangeOfferChanged(GrandExchangeOfferChanged offerChangedEvent) {
+		GrandExchangeOffer offer = offerChangedEvent.getOffer();
+		if(offer.getState() == GrandExchangeOfferState.EMPTY) return;
+		System.out.printf("%nGrand Exchange offer change:%n" +
+						"    Slot: %d, Name: %s, State: %s, Price: %,d / %,d, Quantity: %,d / %,d%n",
+				offerChangedEvent.getSlot(),
+				itemManager.getItemComposition(offer.getItemId()).getName(),
+				offer.getState(),
+				offer.getSpent(),
+				offer.getPrice() * offer.getTotalQuantity(),
+				offer.getQuantitySold(),
+				offer.getTotalQuantity()
+		);
 	}
 
 	@Provides
